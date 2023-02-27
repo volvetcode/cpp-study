3dc01109885ecb0237c231de3b694bdb812a394f
Volvet <darkvolvet@icloud.com>
2023-02-27 10:21:46 +0300

    1

---
+++ book.learning-cpp-by-creating-games/1.firstprogram/firstprogram.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //Import include-output library
+using namespace std; //allows to write cout instead of std::cout
+
+int main() {
+    cout << "Hello, world\n";
+    cout << "I am now a C++ programmer." << endl;
+    return 0;
+}
+
