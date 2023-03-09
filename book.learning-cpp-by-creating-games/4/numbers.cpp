fbd2823dab47d06a94191471f0a401bb7ffa51be
Volvet <darkvolvet@icloud.com>
2023-03-09 17:35:46 +0300

    not working

---
+++ book.learning-cpp-by-creating-games/4/numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+    int x = 4;
+    int y = 9;
+    int z = 900;
+    int s = 50;
+    cout << x += 4 <<endl;
+    cout << y -= 2 <<endl;
+    cout << x /= 2 <<endl;
+    cout << x *= 2 <<endl;
+    return 0;
+}
